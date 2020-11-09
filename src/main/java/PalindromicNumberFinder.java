52264416fe44013d7cd01cbc31922781390d9a7d
kaitlyn grimm <kaitlyngrimm2018@gmail.com>
2020-11-09 16:10:08 +0000

    i return the difference now

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -22,7 +22,7 @@ public class PalindromicNumberFinder {
          palo=1;
        }
      }
-     return palo;
+     return palo-num;
     }
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
